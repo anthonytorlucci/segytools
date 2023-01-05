45fbb43ff0aab109d590dfe16f981ad422135252
anthonytorlucci <anthonytorlucci@gmail.com>
2023-01-05 04:47:51 -0600

    added segytools version

--- src/segytools/__init__.py
+++ src/segytools/__init__.py
@@ -1,5 +1,8 @@
 """segytools"""
 
+from importlib.metadata import version
+__version__ = version("segytools")
+
 from .segy_header_item import SegyHeaderItem
 from .segy_abstract_header import SegyAbstractHeader
 from .segy_file_header import SegyFileHeaderRev2
