259d9c2ddc629d467cce1ae8fb2ce16b613e3791
anthonytorlucci <anthonytorlucci@gmail.com>
2023-01-05 05:12:11 -0600

    test segy header item value

--- tests/test_segy_header_item.py
+++ tests/test_segy_header_item.py
@@ -3,5 +3,5 @@ from context import segytools
 def test_segy_header_item_value_initialization():
     expected = int(0)
     hdr_item = segytools.segy_header_item.SegyHeaderItem(name='example', value=expected)
-    actual = hdr.item.value
+    actual = hdr_item.value
     assert actual == expected, "SegyHeaderItem value incorrectly initialized."
