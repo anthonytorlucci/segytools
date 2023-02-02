3d30d199d868cfacb6e42a2828fe5de9b1640558
anthonytorlucci <anthonytorlucci@gmail.com>
2023-02-02 05:23:02 -0600

    docs action

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2023, Anthony Torlucci'
 author = 'Anthony Torlucci'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.0'  # TODO: get from pyproject.toml
 
 
 # -- General configuration ---------------------------------------------------
