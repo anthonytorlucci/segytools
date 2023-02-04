e4c278f1cfdd65b57301cf3105f8804c60f7069c
anthonytorlucci <anthonytorlucci@gmail.com>
2023-02-04 04:21:11 -0600

    remove explicit version call

--- src/segytools/__init__.py
+++ src/segytools/__init__.py
@@ -1,8 +1,5 @@
 """segytools"""
 
-# from importlib.metadata import version
-# __version__ = version("segytools")
-
 from .segy_abstract_header import SegyAbstractHeader
 from .segy_file_header import SegyFileHeaderRev2
 from .segy_header_item import SegyHeaderItem
