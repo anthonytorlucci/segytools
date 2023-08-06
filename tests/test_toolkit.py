3cfd1a00f67a46ad01d6421eedba585097301367
anthonytorlucci <anthonytorlucci@gmail.com>
2023-08-06 13:39:25 -0500

    issue13 textual header convience function

--- tests/test_toolkit.py
+++ tests/test_toolkit.py
@@ -2,3 +2,7 @@ from context import segytools
 
 int16_zero = b'\x00\x00'
 int32_zero = b'\x00\x00\x00\x00'
+
+# TODO: create a very simple segy file with a few traces; verify the text header with test below.
+# def test_textual_header():
+#     pass
